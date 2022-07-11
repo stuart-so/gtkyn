572d4efcb35cd13fe72f04e381c33300e49f1f35
stuart-so <stuart@silveronyx.com>
2022-07-11 15:48:24 -0600

    backend: trpc, init app and setup connection to planetscale

---
+++ src/server/router/user.router.ts
@@ -0,0 +1,37 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import * as trpc from "@trpc/server";
+import { createUserSchema } from "../schema/user.schema";
+import { createRouter } from "./context";
+
+export const userRouter = createRouter().mutation("register-user", {
+  input: createUserSchema,
+
+  async resolve({ ctx, input }) {
+    const { email, name } = input;
+
+    try {
+      const user = await ctx.prisma.user.create({
+        data: {
+          email,
+          name,
+        },
+      });
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === "P2002") {
+          throw new trpc.TRPCError({
+            code: "CONFLICT",
+            message: "User already exists",
+          });
+        }
+      } else {
+        throw new trpc.TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "This shit broken dawg",
+        });
+      }
+    }
+
+    return user;
+  },
+});
