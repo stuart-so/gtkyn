1c7dba8f7bf0cf083423737708ab54b73514a200
stuart-so <stuart@silveronyx.com>
2022-07-12 10:23:49 -0600

    removing errors

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,10 +3,11 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { userRouter } from "./user.router";
-
+import { exampleRouter } from "./example";
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("users.", userRouter);
+  .merge("users.", userRouter)
+  .merge("example.", exampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
